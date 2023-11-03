158b434ed3d8192eb6491d338cd3c05d64e4c603
padariya-tech <nijpadariya@gmail.com>
2023-11-03 21:25:22 +0530

    counter project use hook

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+        
+
+function App() {
+  let [counter,setcounter]=useState(0)
+  // let counter = 0;
+  function addval() {
+    console.log("counter " ,counter);
+    // counter=counter+1;
+    setcounter(counter+1)
+    
+
+  }
+  function rmvval() {
+    console.log("counter " ,counter);
+    // counter=counter+1;
+    setcounter(counter-1)
+    
+
+  }
+
+  return (
+    <>
+      <h1>handle counter value</h1>
+      <h2 id="inc">counter value: {counter}</h2>
+      <button onClick={addval}>Add Value</button>
+      <br />
+      <button onClick={rmvval}>remove value</button>
+      <footer>{counter}</footer>
+      
+    </>
+  )
+}
+
+export default App
+  
